151eb26736a84e9ef1e8dc03f1634a68db0d7455
Jose Magno <josemagorodrigues98@gmail.com>
2024-03-19 15:38:38 -0300

    [java-api-books](feat):add controller functionalities #3

--- src/main/java/com/jose/java/BooksSpring/Controller/BookController.java
+++ src/main/java/com/jose/java/BooksSpring/Controller/BookController.java
@@ -4,11 +4,16 @@ import com.jose.java.BooksSpring.Domain.Book;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jose.java.BooksSpring.Repository.BookRepository;
+
 
 @RestController
 public class BookController {
@@ -24,11 +29,38 @@ public class BookController {
     }
 
     @SuppressWarnings("null")
+    @PostMapping("/books")
+    public Book create(@RequestBody Book newBook){
+        bookRepository.save(newBook);
+        return newBook;
+    }
+
+    @SuppressWarnings("null")
+    @PutMapping("/books/{id}")
+    public Book update(@RequestBody Book newBook, @PathVariable Long id){
+        return bookRepository.findById(id)
+        .map(book -> {
+          book.setTitle(newBook.getTitle());
+          book.setAuthor(newBook.getAuthor());
+          return bookRepository.save(book);
+        })
+        .orElseGet(() -> {
+          newBook.setId(id);
+          return bookRepository.save(newBook);
+        });
+    }
+
+
+    @SuppressWarnings("null")
     @GetMapping("/books/{id}")
     public Optional<Book> one(@PathVariable Long id){
         Optional<Book> book = bookRepository.findById(id);
 
         return book;
     }
-    
+    @SuppressWarnings("null")
+    @DeleteMapping("books/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+      }
 }
